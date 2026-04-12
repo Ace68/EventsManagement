7ea7c88f57f6879c2fbc584c59e3e8cdbfb0d73f
Alberto Acerbis <alberto.acerbis@gmail.com>
2026-04-12 22:14:06 +0200

    Protected DTO

--- src/Api/Events/EventsManagement.Events.Entities/Dtos/CommunityEventDto.cs
+++ src/Api/Events/EventsManagement.Events.Entities/Dtos/CommunityEventDto.cs
@@ -1,13 +1,35 @@
-﻿using EventsManagement.Shared.Persister;
+﻿using EventsManagement.Events.SharedKernel.CustomTypes;
+using EventsManagement.Shared.DomainIds;
+using EventsManagement.Shared.Persister;
 
 namespace EventsManagement.Events.Entities.Dtos;
 
 public class CommunityEventDto : DtoBase
 {
-    public string EventName { get; set; } = null!;
-    public string EventDescription { get; set; } = null!;
-    public string EventVenue { get; set; } = null!;
-    public DateTime EventDate { get; set; }
+    public string EventName { get; private set; } = null!;
+    public string EventDescription { get; private set; } = null!;
+    public string EventVenue { get; private set; } = null!;
+    public DateTime EventDate { get; private set; }
 
     public virtual ICollection<OrganizersDto> EventOrganizers { get; set; } = null!;
+    
+    protected CommunityEventDto() { }
+    
+    public static CommunityEventDto CreateCommunityEvent(CommunityEventId eventId,
+        EventName eventName, EventDescription eventDescription, EventVenue eventVenue, 
+        EventDate eventDate) 
+        => new (new Guid(eventId.Value), eventName.Value, eventDescription.Value, eventVenue.Value, eventDate.Value);
+
+    private CommunityEventDto(Guid eventId, string eventName, string eventDescription, string eventVenue,
+        DateTime eventDate)
+    {
+        Id = eventId;
+        
+        EventName = eventName;
+        EventDescription = eventDescription;
+        EventVenue = eventVenue;
+        EventDate = eventDate;
+    }
+    
+    public void AddEventOrganizers(IEnumerable<OrganizersDto> organizers) => EventOrganizers = organizers.ToList();
 }
--- src/Api/Events/EventsManagement.Events.Entities/Helpers/CommunityEventHelper.cs
+++ src/Api/Events/EventsManagement.Events.Entities/Helpers/CommunityEventHelper.cs
@@ -10,14 +10,12 @@ public static class CommunityEventHelper
 {
     public static CommunityEventDto ToDto(this CommunityEvent communityEvent)
     {
-        return new CommunityEventDto
-        {
-            EventName = communityEvent.EventName.Value,
-            EventDescription = communityEvent.EventDescription.Value,
-            EventVenue = communityEvent.EventVenue.Value,
-            EventDate = communityEvent.EventDate.Value,
-            EventOrganizers = communityEvent.EventOrganizers.Select(eo => eo.ToDto(communityEvent.EventId)).ToList()
-        };
+        var dto = CommunityEventDto.CreateCommunityEvent(communityEvent.EventId, communityEvent.EventName, communityEvent.EventDescription,
+            communityEvent.EventVenue, communityEvent.EventDate);
+
+        dto.AddEventOrganizers(communityEvent.EventOrganizers.Select(eo => eo.ToDto(communityEvent.EventId)).ToList());
+
+        return dto;
     }
 
     private static OrganizersDto ToDto(this EventOrganizer organizer, CommunityEventId eventId) =>
